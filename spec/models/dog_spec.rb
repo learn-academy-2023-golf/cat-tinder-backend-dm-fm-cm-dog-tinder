a9bd459eac7585459f3af0fd342b6653029df05a
Derek Maly <dmaly18@gmail.com>
2023-12-05 13:26:49 -0800

    final updates to the request specs

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Dog, type: :model do
     expect(dog.errors[:image]).to_not be_empty
   end
   it "should enjoy something longer than 10 characters" do
-    dog = Dog.create(name: 'Buster', enjoys: 'Barking' age: 1, image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Ffacts%2Fdomestic-dog&psig=AOvVaw3-j9wFcbZC8vIv4a9nd1jW&ust=1701546238962000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJig--r_7oIDFQAAAAAdAAAAABAK')
+    dog = Dog.create(name: 'Buster', enjoys: 'Barking', image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Ffacts%2Fdomestic-dog&psig=AOvVaw3-j9wFcbZC8vIv4a9nd1jW&ust=1701546238962000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJig--r_7oIDFQAAAAAdAAAAABAK')
     expect(dog.errors[:enjoys]).to_not be_empty
   end
 end
